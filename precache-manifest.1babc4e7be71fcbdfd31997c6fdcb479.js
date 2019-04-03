f8c145025066fa45ad2e9201ec86f4d0eeff6941
caliquerecuto <caliquerecuto@gmail.com>
2019-04-03 11:12:59 +0200

    Updates

---
+++ precache-manifest.1babc4e7be71fcbdfd31997c6fdcb479.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c716382d87f5dd62ea407ce9cf1550a",
+    "url": "/react-portafolio/static/media/foto2.3c716382.png"
+  },
+  {
+    "revision": "76f60d0e798c8d4c4e9c",
+    "url": "/react-portafolio/static/css/main.a0338795.chunk.css"
+  },
+  {
+    "revision": "476aa14aac53bbdde42cb175231f544f",
+    "url": "/react-portafolio/static/media/foto.476aa14a.png"
+  },
+  {
+    "revision": "01752d879cf75d0307bb",
+    "url": "/react-portafolio/static/js/1.01752d87.chunk.js"
+  },
+  {
+    "revision": "7897d507f12159d97c6d",
+    "url": "/react-portafolio/static/js/runtime~main.7897d507.js"
+  },
+  {
+    "revision": "d993fabdf38cc35905b81cbe9d080c37",
+    "url": "/react-portafolio/static/media/about.d993fabd.png"
+  },
+  {
+    "revision": "592e18763df103dd41c5be274cfe3f07",
+    "url": "/react-portafolio/static/media/portafolioinfo.592e1876.png"
+  },
+  {
+    "revision": "0de59a518082f87537cc58f1f13e41a4",
+    "url": "/react-portafolio/static/media/logoc.0de59a51.png"
+  },
+  {
+    "revision": "b3c8394cd7e82523a118757f6954f46a",
+    "url": "/react-portafolio/static/media/Car2.b3c8394c.png"
+  },
+  {
+    "revision": "76f60d0e798c8d4c4e9c",
+    "url": "/react-portafolio/static/js/main.76f60d0e.chunk.js"
+  },
+  {
+    "revision": "aa9cb7b427aba2911238cd3a5c56b27d",
+    "url": "/react-portafolio/static/media/skylab.aa9cb7b4.png"
+  },
+  {
+    "revision": "d776d09b0a45a7a33f927ddf4b2ae881",
+    "url": "/react-portafolio/static/media/logoreact.d776d09b.png"
+  },
+  {
+    "revision": "6e1acda72e4e28ea3ed314dba393d1d9",
+    "url": "/react-portafolio/static/media/logosass.6e1acda7.png"
+  },
+  {
+    "revision": "42feaf06a541943755f15cd1b0e26485",
+    "url": "/react-portafolio/static/media/skills.42feaf06.png"
+  },
+  {
+    "revision": "1b7cde609729f8123d67a6e791762141",
+    "url": "/react-portafolio/static/media/linkedin2.1b7cde60.png"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/react-portafolio/static/media/logoreact2.7c0fdc49.png"
+  },
+  {
+    "revision": "f367dd1a085c0aa8f78a525a6acfaf3f",
+    "url": "/react-portafolio/static/media/portafolionew.f367dd1a.png"
+  },
+  {
+    "revision": "01752d879cf75d0307bb",
+    "url": "/react-portafolio/static/css/1.06dabbde.chunk.css"
+  },
+  {
+    "revision": "e6cd9f313b646bc38a2c5836bd1919d7",
+    "url": "/react-portafolio/index.html"
+  }
+];
